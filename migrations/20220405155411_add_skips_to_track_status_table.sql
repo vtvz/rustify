dd2a78ee73f8a6719e3f02575a1ebc6b0c5df28c
Vitaly <vtvz.ru@gmail.com>
2023-11-06 21:23:09 +0300

    Migrate to Postgres

--- migrations/20220405155411_add_skips_to_track_status_table.sql
+++
@@ -1,3 +0,0 @@
--- Add migration script here
-alter table track_status
-    add skips integer default 0;
--- migrations/20220413192435_create_user_table.sql
+++ migrations/20220413192435_create_user_table.sql
@@ -1,13 +1,20 @@
--- Add migration script here
-create table user
+create table "user"
 (
-    id                 text not null
-        constraint user_pk
-            primary key,
-    name               text not null default 'unknown',
-    removed_playlists  int  not null default 0,
-    removed_collection int  not null default 0,
-    status             text          default 'active' not null,
-    created_at         text          default (datetime('now', 'localtime')) not null,
-    updated_at         text          default (datetime('now', 'localtime')) not null
+  id                 text      not null primary key,
+  name               text      not null default 'unknown',
+  removed_playlists  int       not null default 0,
+  removed_collection int       not null default 0,
+  status             text      not null default 'active',
+  created_at         timestamp not null default current_timestamp,
+  updated_at         timestamp not null default current_timestamp,
+  playing_track      text      not null default '',
+
+  lyrics_checked     int not null default 0,
+  lyrics_genius      int not null default 0,
+  lyrics_musixmatch  int not null default 0,
+  lyrics_profane     int not null default 0
 );
+
+create unique index user_id_uindex on "user" (id);
+
+create index user_status_index on "user" (status);
